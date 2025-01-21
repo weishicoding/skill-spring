f6d06442f9848ce08d3a994e8bb7a5ea52383845
weishicoding <sanwind0305@gmail.com>
2025-01-21 21:28:11 +0200

    Add footer and  header

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="mx-auto flex max-w-5xl flex-col px-8 pb-8 border-t">
+      <div className="mt-12 flex justify-between">
+        <div className="text-sm text-gray-500 dark:text-gray-400">
+          <p>&copy; 2025 - All rights reserved.</p>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,15 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="fixed inset-x-0 top-0 z-40 bg-background/80 shadow-sm saturate-[1.8] backdrop-blur-[10px] dark:saturate-100">
+      <div className="mx-auto flex h-[60px] max-w-5xl items-center justify-between px-8">
+        Header
+      </div>
+    </header>
+  );
 };
 
 export default Header;
